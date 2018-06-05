d07bdbfd70e662a0429a194ef73d34a08b5e7406
Andy <andy@andysouter.net>
2018-06-05 22:48:35 +0100

    minor cleanup

--- arbitrait.js
+++ arbitrait.js
@@ -1,14 +1,16 @@
 const findFirst = (ar, cb) => {
     for (var i = 0; i < ar.length; i++) if (cb(ar[i])) return ar[i];
-}
+};
 
 const createTrait = (funcs) => {
     var symbol = Symbol();
     var impls = {}; // { ConstrName: [ [constr, fns], ... ] }
+    const findImpl = constr => {
+        const pair = impls[constr.name] && findFirst(impls[constr.name], pair => pair[0] === constr);
+        return pair && pair[1];
+    };
     var trait = obj => {
-        // find impl for obj's constr
-        var implementedFuncs = impls[obj.constructor.name]
-            && findFirst(impls[obj.constructor.name], implPair => implPair[0] == obj.constructor);
+        var implementedFuncs = findImpl(obj.constructor);
         // haven't decided whether to allow implicit global impls with default funcs
         // leaning towards "no" for consistency
         if (implementedFuncs === undefined) {
@@ -16,7 +18,7 @@ const createTrait = (funcs) => {
         }
         // if allowing implicit impl...
         // implementedFuncs = implementedFuncs == undefined ? {} : implementedFuncs[1];
-        implementedFuncs = implementedFuncs[1];
+
         // combine with default funcs
         var combined = { ...funcs, ...implementedFuncs };
         // bind
@@ -35,18 +37,16 @@ const createTrait = (funcs) => {
         if(trait.implementedFor(constructor)) throw new Error("Trait already implemented for " + constructor.name);
         // allow trait(Sub,Parent)
         if (typeof implFuncs == "function") {
-            var pair = impls[implFuncs.name] === undefined
-                ? undefined
-                : findFirst(impls[implFuncs.name], pair => pair[0] == implFuncs);
-            if (!pair) throw new Error("Trait not implemented for " + implFuncs.name);
-            trait.implement(constructor, pair[1]);
+            const srcImpl = findImpl(implFuncs);
+            if (!srcImpl) throw new Error("Trait not implemented for " + implFuncs.name);
+            trait.implement(constructor, srcImpl);
             return;
         }
         Object.keys(implFuncs).forEach(k => {
             if (!funcs.hasOwnProperty(k)) throw new Error(k + " does not exist in this trait");
         });
         Object.keys(funcs).forEach(k => {
-            // make sure any abstract fn is provided
+            // make sure all abstract fns provided
             if (typeof funcs[k] !== "function" && !implFuncs[k])
                 throw new Error("Trait implementation for " + constructor.name + " missing `" + k + "`");
             // check signature
@@ -55,10 +55,10 @@ const createTrait = (funcs) => {
                     ? funcs[k].length
                     : Number(funcs[k]);
                 if (implFuncs[k].length !== expectedParamCount)
-                    throw new Error(`${k} implementation should take ${expectedParamCount} arguments`);
+                    throw new Error(`${k} implementation should take ${expectedParamCount} argument(s)`);
             }
         });
-        if (impls[constructor.name] == undefined) impls[constructor.name] = [];
+        if (impls[constructor.name] === undefined) impls[constructor.name] = [];
         impls[constructor.name].push([constructor, implFuncs]);
     };
     trait[symbol] = symbol;
